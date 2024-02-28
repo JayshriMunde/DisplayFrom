325db69af1cd0b39e9230073f49f1d953da88dc2
Jayshri <jayshrimunde97@gmail.com>
2024-02-28 23:27:42 +0530

    displayform

---
+++ src/FormDisplay/displayForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./displayForm.css";
+const DisplayForm = () => {
+  const [Fname, setFirstName] = useState("");
+  const [Lname, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const fullName = `${Fname} ${Lname}`;
+    setFullName(fullName);
+  };
+
+  return (
+    <div className="displayform">
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          First Name:
+          <input
+            name="firstname"
+            value={Fname}
+            required
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <br />
+          Last Name:
+          <input
+            name="lastname"
+            value={Lname}
+            required
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {fullName && (
+        <div>
+          <label>Full Name: {fullName}</label>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DisplayForm;
